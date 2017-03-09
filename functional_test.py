61dc4a153f6c5565a7aa8ee2599fb7d8eaa97b71
Sergio Alonso <sergio@sergioalonso.es>
2017-03-09 12:44:45 +0100

    ft: check if django is installed

---
+++ functional_test.py
@@ -0,0 +1,8 @@
+"""Check that we have Django installed."""
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
