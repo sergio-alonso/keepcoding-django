e22222285a18481338d24e97560bedea183e3361
Sergio Alonso <sergio@sergioalonso.es>
2017-03-09 22:18:13 +0100

    ft: write a story

--- functional_test.py
+++ functional_test.py
@@ -2,7 +2,17 @@
 
 from selenium import webdriver
 
+
 browser = webdriver.Firefox()
+
+# Alice has heard about a cool new online app.
+# She goes to check out its homepage.
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention resource lists
+assert 'Recursos' in browser.title
+
+# And the page lists last post published by another users
+
+# Satisfied, she goes back to sleep
+browser.quit()
