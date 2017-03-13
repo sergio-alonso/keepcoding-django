a24ebb58af82482118bc8e6449d95d815076cd7d
Sergio Alonso <sergio@sergioalonso.es>
2017-03-13 20:04:43 +0100

    new form for blog posts

---
+++ blog/forms.py
@@ -0,0 +1,25 @@
+"""Django blog forms."""
+from django import forms
+
+from blog.models import Post
+
+EMPTY_POST_TITLE_ERROR = "You can't have an empty post title"
+
+
+class PostForm(forms.models.ModelForm):
+    """Post form."""
+
+    class Meta:
+        """Meta."""
+
+        model = Post
+        fields = ('title',)
+        widgets = {
+            'title': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a post title',
+                'class': 'post-title',
+            }),
+        }
+        error_messages = {
+            'title': {'required': EMPTY_POST_TITLE_ERROR}
+        }
---
+++ blog/tests/tests_forms.py
@@ -0,0 +1,21 @@
+"""Django blog app test forms."""
+from django.test import TestCase
+from blog.forms import PostForm, EMPTY_POST_TITLE_ERROR
+
+
+class PostFormTest(TestCase):
+    """Test suite: post form test."""
+
+    def test_form_post_input_has_placeholder_and_css_classes(self):
+        """Test case: form renders post title input."""
+        form = PostForm()
+        self.assertIn('placeholder="Enter a post title"', form.as_p())
+        self.assertIn('class="post-title"', form.as_p())
+
+    def test_form_validation_for_blank_posts(self):
+        """Test case: form validation for blank posts."""
+        form = PostForm(data={'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['title'], [EMPTY_POST_TITLE_ERROR]
+        )
