dd26b5aef959acfcce3a8ad0632b82ccb85c613d
Sergio Alonso <sergio@sergioalonso.es>
2017-03-14 23:07:57 +0100

    ft: decorator for explicit wait

--- functional_tests/functional_test.py
+++ functional_tests/functional_test.py
@@ -6,11 +6,25 @@ import selenium.webdriver as webdriver
 import selenium.webdriver.common.keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
+MAX_WAIT = 10
+
+
+def wait(fn):
+    """Wait."""
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, selenium.common.exceptions.WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     """Functional Tests."""
-
-    MAXWAIT = 10
 
     @classmethod
     def setUp(self):
@@ -21,47 +35,32 @@ class FunctionalTest(StaticLiveServerTestCase):
         """Test case tearDown."""
         self.browser.quit()
 
+    @wait
+    def wait_for(self, fn):
+        """Wait for."""
+        return fn()
+
+    @wait
     def wait_for_row_in_post_table(self, row_text):
         """Check if a text exists as table row."""
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_class_name('post-list')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, selenium.common.exceptions.WebDriverException) as e:
-                if time.time() - start_time > self.MAXWAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def wait_for(self, fn):
-        """Check if a text exists as table row."""
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, selenium.common.exceptions.WebDriverException) as e:
-                if time.time() - start_time > self.MAXWAIT:
-                    raise e
-                time.sleep(0.5)
+        table = self.browser.find_element_by_class_name('post-list')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def get_post_input_box(self):
         """Get post title."""
         return self.browser.find_element_by_id('id_title')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         """Wait to be logged in."""
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         """Wait to be logged out."""
-        self.wait_for(
-            lambda: self.browser.find_element_by_name('email')
-        )
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
