c490d1f7716c4ce9ffa0fdec92d7d786f923456e
Sergio Alonso <sergio@sergioalonso.es>
2017-03-19 22:56:27 +0100

    urls: post

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,4 +5,5 @@ from blogs import views
 
 urlpatterns = [
     url(r'^(?P<user_email>[\w.@+-]+)/$', views.blog, name='blog'),
+    url(r'post-save', views.post_save, name='post_save'),
 ]
--- keepcoding_django/urls.py
+++ keepcoding_django/urls.py
@@ -22,7 +22,7 @@ from blogs import views as blogs_views
 
 urlpatterns = [
     url(r'^$', blogs_views.home, name='home'),
-    url(r'^new-post$', blogs_views.post, name='post'),
+    url(r'^new-post$', blogs_views.post_create , name='post_create'),
     url(r'^blogs/', include(blogs_urls)),
     url(r'^accounts/', include(accounts_urls)),
 ]
