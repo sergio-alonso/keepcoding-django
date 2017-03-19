a9467d6137241dc500b007913bc1792e94fa74df
Sergio Alonso <sergio@sergioalonso.es>
2017-03-19 20:56:07 +0100

    Post model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post model."""
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.TextField(default='', unique=True)
+
+    def __str__(self):
+        """__str___."""
+        return self.title
