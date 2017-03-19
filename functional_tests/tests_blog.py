bb6b5c173a6522b95842836ec12318e1ca1e5282
Sergio Alonso <sergio@sergioalonso.es>
2017-03-19 01:45:16 +0100

    ft: add user blog tests

--- functional_tests/tests_blog.py
+++ functional_tests/tests_blog.py
@@ -9,16 +9,35 @@ class BlogTest(FunctionalTest):
     def test_logged_in_users_blog_are_saved_as_my_blog(self):
         """Test case: logged in users blog are saved as my blog."""
         # Alice is a logged-in user
+
         self.create_pre_authenticated_session('alice@example.com')
 
-        # She goes to the home page and starts a blog post
-        #self.browser.get(self.live_server_url)
-        #self.add_blog_post('First post')
-        #self.add_blog_post('Second post')
-        #first_post_url = self.browser.current_url
+        # She goes to her blog page
 
-        # She notices a "My blog" link, for the first time.
-        #self.browser.find_element_by_link_text('My blog').click()
+        self.browser.get(self.live_server_url + '/blogs/alice@example.com/')
+
+        # She notices a "My blog" link, that takes her to the same page! :)
+
+        self.browser.find_element_by_link_text('My blog').click()
+
+        blog_url = self.browser.current_url
+        self.assertRegex(blog_url, '/blogs/.+')
+        self.assertIn('alice@example.com', blog_url)
+
+        # And a message of no post at the bottom.
+
+        self.wait_for(
+            lambda: self.browser.find_element_by_class_name('posts-count')
+        )
+        posts_count = self.browser.find_element_by_class_name('posts-count').text
+        self.assertEqual('No posts', posts_count)
+
+        # She decides to start a post, just to see.
+        self.browser.find_element_by_link_text('Create a new post')
+        #self.browser.find_element_by_link_text('Create a new post').click
+
+        # A new page appears, with a form to create a post
+        #redirect('/new-post')
 
         # She sees that her blog is in there, named according to its
         # first blog post
@@ -30,24 +49,9 @@ class BlogTest(FunctionalTest):
         #    lambda: self.assertEqual(self.browser.current_url, first_post_url)
         #)
 
-        # She decides to start another post, just to see
-        #self.browser.get(self.live_server_url)
-        #self.add_blog_post('Third post')
-        #second_post_url = self.browser.current_url
-
-        # Under "My blog", her new post appears
-        #self.browser.find_element_by_link_text('My blog').click()
-        #self.wait_for(
-        #    lambda: self.browser.find_element_by_link_text('Third post')
-        #)
-        #self.browser.find_element_by_link_text('third post').click()
-        #self.wait_for(
-        #    lambda: self.assertEqual(self.browser.current_url, second_post_url)
-        #)
-
-        # She logs out.  The "My blog" option disappears
-        #self.browser.find_element_by_link_text('Log out').click()
-        #self.wait_for(lambda: self.assertEqual(
-        #    self.browser.find_elements_by_link_text('My blog'),
-        #    []
-        #))
+        # She logs out. The "My blog" option disappears
+        self.browser.find_element_by_link_text('Log out').click()
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_elements_by_link_text('My blog'),
+            []
+        ))
