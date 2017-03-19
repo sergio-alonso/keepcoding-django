54f0b413911f71bc22604279c99bb52aff3f0779
Sergio Alonso <sergio@sergioalonso.es>
2017-03-19 22:55:56 +0100

    views: post

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+"""Blog views."""
+
+from django.shortcuts import render, redirect
+
+from blogs.forms import PostForm, NewPostForm
 
 def home(request):
+    """Home view."""
     return render(request, 'home.html')
 
+
 def blog(request, user_email):
+    """Blog view."""
     return render(request, 'blog.html')
 
-def post(request):
-    return render(request, 'post.html')
+
+def post_create(request):
+    """Post create view."""
+    return render(request, 'post_create.html', {'form': PostForm()})
+
+
+def post_save(request):
+    """Post save view."""
+    form = NewPostForm(data=request.POST)
+    if form.is_valid():
+        post = form.save(owner=request.user)
+        return render(request, 'blog.html')
+    return render(request, 'post_create.html', {'form': NewPostForm()})
