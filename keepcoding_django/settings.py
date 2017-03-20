b31d3e317229520455bc69975879a4a4019300a7
Sergio Alonso <sergio@sergioalonso.es>
2017-03-20 23:45:06 +0100

    Use standard django permissions

--- keepcoding_django/settings.py
+++ keepcoding_django/settings.py
@@ -152,3 +152,11 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
