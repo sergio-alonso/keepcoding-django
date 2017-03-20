5a4a54e36de3cd57ea4c2f9b2f7d79d918129512
Sergio Alonso <sergio@sergioalonso.es>
2017-03-20 02:11:48 +0100

    urls: blogs list

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^(?P<user_email>[\w.@+-]+)/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^(?P<user_email>[\w.@+-]+)/$', views.blog, name='blog'),
     url(r'post-save', views.post_save, name='post_save'),
+    url(r'^$', views.BlogsListView.as_view(), name='blogs_list'),
 ]
