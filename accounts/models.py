c142e3b46c36da2781afebac685b126ef9e77b46
Sergio Alonso <sergio@sergioalonso.es>
2017-03-24 18:40:37 +0100

    models: accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,56 +1,108 @@
 """Django accounts models."""
 
 from django.contrib import auth
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.urlresolvers import reverse
 
 import uuid
+import re
 
 auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
 
+
+class UserManager(auth.models.BaseUserManager):
+    """User Manager."""
+
+    def create_user(self, email, password=None):
+        return
+        print(email, password)
+        """Create user."""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password = password
+        )
+
+        # Only example.com emails are admins
+        domain = re.search("@[\w.]+", email)
+        if domain == '@example.com':
+            user.is_admin = True
+
+        if password == None:
+            user.password = 'supersecret'
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create superuser."""
+        self.create_user(email, password)
 
 class User(models.Model):
     """User model."""
 
     email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=50)
+
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
     is_anonymous = False
     is_authenticated = True
 
+    def get_username(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    def check_password(self, password):
+        return self.password == password
+
+    def set_password(self, password):
+        self.password = password
+
+    def get_absolute_url(self):
+        return reverse('api:user-detail', args=[self.email])
+
+    def get_blog_url(self):
+        return reverse('api:blogs-detail', args=[self.email])
 
 class Token(models.Model):
     """Token model."""
 
     email = models.EmailField()
     uid = models.CharField(default=uuid.uuid4, max_length=40)
-
-
-# class ListUserManager(auth.BaseUserManager):
-#     """List User Manager."""
-
-#     def create_user(self, email):
-#         """Create user."""
-#         ListUser.objects.create(email=email)
-
-#     def create_superuser(self, email, password):
-#         """Create superuser."""
-#         self.create_user(email)
-
-
-# class ListUser(auth.AbstractBaseUser, auth.PermissionsMixin):
-#     """List User."""
-
-#     email = models.EmailField(primary_key=True)
-#     USERNAME_FIELD = 'email'
-#     # REQUIRED_FIELDS = ['email', 'height']
-#     objects = ListUserManager()
-
-#     @property
-#     def is_staff(self):
-#         """Check if is staff."""
-#         return self.email == 'admin@example.com'
-
-#     @property
-#     def is_active(self):
-#         """Check if is active."""
-#         return True
