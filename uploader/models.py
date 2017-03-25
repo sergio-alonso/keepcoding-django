1d296b3a4e059e8a03395ba64eddf02039d2a4f3
Sergio Alonso <sergio@sergioalonso.es>
2017-03-26 00:40:57 +0100

    uploader: models

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Image(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    file = models.FileField(upload_to=settings.MEDIA_ROOT)
