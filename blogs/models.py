b51ab3ef1a8b0aa8b9ed3d5247bf874b20df272f
Sergio Alonso <sergio@sergioalonso.es>
2017-03-27 00:32:40 +0200

    models: add category model

--- blogs/models.py
+++ blogs/models.py
@@ -2,7 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 
-import tagulous.models
+class Category(models.Model):
+
+    name = models.CharField(max_length=128, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     """Post model."""
@@ -12,7 +18,7 @@ class Post(models.Model):
     summary = models.TextField(default='', null=True, blank=True)
     description = models.TextField(default='', null=True, blank=True)
     imagen = models.URLField(default='', null=True, blank=True)
-    category = tagulous.models.TagField(force_lowercase=True, blank=True)
+    category = models.ManyToManyField(Category)
     published_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
