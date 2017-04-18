5af16d48f0c64d4d53b128fd60537bdaa34ca452
Sergio Alonso <sergio@sergioalonso.es>
2017-04-18 21:29:44 +0200

    fix reused namespace in urls

--- keepcoding_django/urls.py
+++ keepcoding_django/urls.py
@@ -20,7 +20,7 @@ from accounts import urls as accounts_urls
 from blogs import urls as blogs_urls
 
 from blogs import views as blogs_views
-from blogs import api
+from blogs import api as blogs_api
 
 from uploader import api_urls as uploader_api
 
@@ -31,6 +31,6 @@ urlpatterns = [
     url(r'^blogs/', include(blogs_urls)),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v1/', include(api.router.urls, namespace='api')),
-    url(r'^api/v1/', include(uploader_api.router.urls, namespace='api')),
+    url(r'^api/v1/', include(blogs_api.router.urls, namespace='api')),
+    url(r'^api/v1/', include(uploader_api.router.urls, namespace='api:uploader')),
 ]
