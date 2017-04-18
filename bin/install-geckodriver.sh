ca07a086b4a254fa5f217af2cffa430e33fb2bf1
Sergio Alonso <sergio@sergioalonso.es>
2017-04-18 16:23:10 +0200

    scripts: remove install geckodriver steps

--- bin/install-geckodriver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-curl -L https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz | sudo tar zxv -C /usr/local/bin
-
-exit 0
